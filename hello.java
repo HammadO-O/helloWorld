a199eaea65a8f104643c73fbcd72765cf1327126
HammadSP19 <hammad@gmail.com>
2023-01-08 19:20:23 +0500

    New commits wiht 2 loops

--- hello.java
+++ hello.java
@@ -3,7 +3,11 @@ public class hello{
     public static void main(String[] args){
         
         for(int i = 1; i<=5;i++){
-            System.out.println("Hello World ... ! " + i);
+            System.out.println("hammad loves hamza ... ! " + i);
+        }
+        System.out.println("--------------------------------------------------------");
+        for(int i =0;i<=2;i++){
+            System.out.println("u want diamonds ? contact furqan ... ! " + i);
         }
 
     }
